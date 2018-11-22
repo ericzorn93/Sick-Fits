d25ace316b8e2f5c680c35aa38505e59702ba4f2
Eric Zorn <zornwebdev@gmail.com>
2018-11-21 23:34:59 -0500

    Included Server Side CSS Pre-rendering with _document.js file

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b2c155db9d915dfccaf3";
+/******/ 	var hotCurrentHash = "ad4a5e794aa7d1262fdf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
