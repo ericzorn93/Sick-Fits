f37b7d88813dc7474c9d8c306c6266b621bb8b0b
ericzorn93 <zornwebdev@gmail.com>
2018-11-24 13:13:42 -0500

    Added Querying and Creating Item in GraphQL. Commented out createdAt, updatedAt, and user in prisma.graphql. Commented out user in itemCreateInput in prisma.graphql

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,15 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    // Create a dog
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      },
+    }, info);
+
+    return item;
+
   }
 };
 
