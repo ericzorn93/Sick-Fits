0e98f4e421c09c9cff0387b0c7225ff329a4acb6
Eric Zorn <zornwebdev@gmail.com>
2018-11-25 00:03:02 -0500

    Updated packages for both frontend and backend

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8f0b8e0807e36e66059d";
+/******/ 	var hotCurrentHash = "475ac25545ac29641b0c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
