cf727338247fc276ecb19a804345d56122bf557e
Eric Zorn <zornwebdev@gmail.com>
2018-11-25 10:11:24 -0500

    Used prettier extension to make code look much neater

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,15 +1,17 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
-    
-    const item = await ctx.db.mutation.createItem({
-      data: {
-        ...args
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
       },
-    }, info);
+      info
+    );
 
     return item;
-
   }
 };
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,7 @@
-const { forwardTo } = require('prisma-binding');
+const { forwardTo } = require("prisma-binding");
 
 const Query = {
-  items: forwardTo('db'),
+  items: forwardTo("db")
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
