5f6593d5a4ce39800c60d533560bec52b166ace1
Eric Zorn <zornwebdev@gmail.com>
2018-11-28 22:24:46 -0500

    Update metadata

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2bc4da63b967084f8ac7";
+/******/ 	var hotCurrentHash = "283a5f481695e962bb5f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
